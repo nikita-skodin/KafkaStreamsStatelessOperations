009ddffe7305dd037370f8ff77b51c40572740d1
nikita-skodin <skodin36@gmail.com>
2024-02-07 16:57:14 +0300

    models added

---
+++ KafkaStreams/src/main/java/com/skodin/kafkastreamstestproject/models/ParsedVoiceCommand.java
@@ -0,0 +1,17 @@
+package com.skodin.kafkastreamstestproject.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ParsedVoiceCommand {
+    private String id;
+    private String textCommand;
+    private Double probability;
+    private String language;
+}
